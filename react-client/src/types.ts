4af67f41ca0f2e686b883dd8597513395e851d1d
LenerMoraga <lener.baez@gaf.energy>
2023-09-04 23:37:23 -0600

    fix Message Type

--- react-client/src/types.ts
+++ react-client/src/types.ts
@@ -2,6 +2,6 @@
 
 import type { GetUsersQuery } from "./graphql/generated";
 
-export type Message = GetUsersQuery["users"][0]["messages"];
+export type Message = GetUsersQuery["users"][0]["messages"][0];
 
 export type User = GetUsersQuery["users"][0];
